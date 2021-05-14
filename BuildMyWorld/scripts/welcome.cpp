9401f443e2765d7bee8900d1ba9d2c4937c1445d
sudujr <sjsudharshan16@gmail.com>
2021-05-14 17:02:06 +0000

    Build My World

---
+++ BuildMyWorld/scripts/welcome.cpp
@@ -0,0 +1,18 @@
+#include <gazebo/gazebo.hh>
+
+namespace gazebo
+{
+  class WorldPluginMyRobot : public WorldPlugin
+  {
+    public: WorldPluginMyRobot() : WorldPlugin()
+            {
+              printf("Welcome to My First Gazebo World!\n");
+              printf("Enjoy Your Stay!\n");
+            }
+
+    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+            {
+            }
+  };
+  GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyRobot)
+}
